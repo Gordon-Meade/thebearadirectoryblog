867cd083604bac4e89ed0bf89fe64c2764ef5056
Gordon Meade <gordonmeade@gmail.com>
2024-03-22 12:40:22 +0000

    Update to Models.py, to tidy comment model

--- blog/models.py
+++ blog/models.py
@@ -38,3 +38,4 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
+
--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path("post/<int:pk>/", views.blog_detail, name="blog_detail"),
     path("category/<category>/", views.blog_category, name="blog_category"),
     path("blog_info/", views.info_view, name="blog_info"),
-    path("index/", views.blog_index, name="blog_index"),
+  
 ]
